e8e7384849a3fdf44e3d4ec3122f0209dd02bfd4
Yapteva Marina <MYapteva@croc.ru>
2024-02-24 10:12:23 +0300

    избавились от дублирования кода

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,64 +1,17 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GroupCreationTests {
-    private static WebDriver driver;
-
-    private boolean IsElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-    }
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {
-            driver = new ChromeDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(1368, 770));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void canCreateGroup() {
-        if (!IsElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("group1");
-        driver.findElement(By.name("group_header")).sendKeys("header");
-        driver.findElement(By.name("group_footer")).sendKeys("footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupsPage();
+        createGroup("name", "header", "footer");
     }
-
 
     @Test
     public void canCreateGroupWithEmptyName() {
-        if (!IsElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
-        }
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-//    driver.findElement(By.name("group_name")).sendKeys("group1");
-        driver.findElement(By.name("group_header")).sendKeys("header");
-        driver.findElement(By.name("group_footer")).sendKeys("footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+        openGroupsPage();
+        createGroup("", "header", "footer");
     }
 }
--- addressbook_web_tests/src/test/java/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -1,59 +1,12 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
 
-public class GroupRemovalTests {
-    private WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        if (driver == null) {
-            driver = new ChromeDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(1368, 770));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.findElement(By.linkText("Logout")).click();
-        driver.quit();
-    }
-
+public class GroupRemovalTests extends TestBase {
     @Test
     public void canRemoveGroup() {
-        if (!IsElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
+        openGroupsPage();
+        if (isGroupPresent()) {
+            createGroup("group1", "header", "footer");
         }
-        if (!IsElementPresent(By.name("selected[]"))) {
-            driver.findElement(By.name("new")).click();
-            driver.findElement(By.name("group_name")).click();
-            driver.findElement(By.name("group_name")).sendKeys("group1");
-            driver.findElement(By.name("group_header")).sendKeys("header");
-            driver.findElement(By.name("group_footer")).sendKeys("footer");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.linkText("group page")).click();
-        }
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    private boolean IsElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
+        removeGroup();
     }
 }
