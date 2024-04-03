a37810f6ef1258cc53fe74493296ede1cf7c5806
Yapteva Marina <MYapteva@croc.ru>
2024-04-03 23:34:57 +0300

    добавлен нужный каталог для мантиса

---
+++ mantis_tests/src/test/java/mantis/manager/SessionHelper.java
@@ -0,0 +1,21 @@
+package mantis.manager;
+
+import org.openqa.selenium.By;
+
+public class SessionHelper extends HelperBase {
+    public SessionHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+
+    public void login(String user, String password) {
+        type(By.id("username"), user);
+        click(By.cssSelector("input[type='submit']"));
+        type(By.id("password"), password);
+        click(By.cssSelector("input[type='submit']"));
+    }
+
+    public boolean isLoggedIn() {
+        return isElementPresent(By.cssSelector("span.user-info"));
+    }
+}
