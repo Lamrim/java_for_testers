a7ec003468575c23bda07acffc9a63a6ddfbe2e6
Yapteva Marina <MYapteva@croc.ru>
2024-04-13 00:38:57 +0300

    добавлен конфиг для использования selenium server

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,8 +5,15 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -19,14 +26,23 @@ public class ApplicationManager {
 
     private Properties properties;
 
-    public void init(String browser, Properties properties) {
+    public void init(String browser, Properties properties) throws MalformedURLException, URISyntaxException {
         this.properties = properties;
 
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("chrome".equals(browser)) {
-                driver = new ChromeDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URI(seleniumServer).toURL(), new ChromeOptions());
+                } else {
+                    driver = new ChromeDriver();
+                }
             } else if ("firefox".equals(browser)) {
-                driver = new FirefoxDriver();
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URI(seleniumServer).toURL(), new FirefoxOptions());
+                } else {
+                    driver = new FirefoxDriver();
+                }
             } else {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 public class TestBase {
@@ -14,7 +15,7 @@ public class TestBase {
     protected static ApplicationManager app;
 
     @BeforeEach
-    public void setUp() throws IOException {
+    public void setUp() throws IOException, URISyntaxException {
         if (app == null) {
             var properties = new Properties();
             properties.load(new FileReader(System.getProperty("environment", "local.properties")));
